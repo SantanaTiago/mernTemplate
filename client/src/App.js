8017a24789038050eaecd9f43a38f7a55a8658c0
SantanaTiago <tiagocostasantana@gmail.com>
2020-10-14 17:34:13 +0100

    initial commit. Simple backend and simple frontend.

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import Navigation from './components/layout/navigation';
+import Header from './components/layout/header';
+import Features from './components/layout/features';
+import About from './components/layout/about';
+import Services from './components/layout/services';
+import Team from './components/layout/Team';
+import Contact from './components/layout/contact';
+import JsonData from './data/data.json';
+
+export class App extends Component {
+  state = {
+    landingPageData: {},
+  }
+  getlandingPageData() {
+    this.setState({landingPageData : JsonData})
+  }
+
+  componentDidMount() {
+    this.getlandingPageData();
+  }
+
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <Header data={this.state.landingPageData.Header} />
+        <Features data={this.state.landingPageData.Features} />
+        <About data={this.state.landingPageData.About} />
+        <Services data={this.state.landingPageData.Services} />
+        <Team data={this.state.landingPageData.Team} />
+        <Contact data={this.state.landingPageData.Contact} />
+      </div>
+    )
+  }
+}
+
+export default App;
